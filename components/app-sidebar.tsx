0af1cdc08682f3319b9a74d601d8a12c5bcb8092
loulibre <info@librepath.com>
2025-03-02 22:14:36 -0800

    fix icon style for vercel
    fix icon style for vercel build

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -325,7 +325,7 @@ import {
                     <SidebarMenuItem>
                       <CollapsibleTrigger asChild>
                         <SidebarMenuButton tooltip={item.title}>
-                          {item.icon && <item.icon />}
+                        {item.icon && <item.icon className="sidebar-icon" />}
                           <span>{item.title}</span>
                           <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
                         </SidebarMenuButton>
